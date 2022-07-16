90a173b1a75769fbcc08b0939f17f30e78a70b3e
chilubagh <ibrahim.dauda@integrify.io>
2022-07-17 00:06:21 +0300

    added data ingestion code to pipline.py

--- producer/producer/pipelines.py
+++ producer/producer/pipelines.py
@@ -5,9 +5,12 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+from pipeline.redis_client import RedisClient
 
 
 class ProducerPipeline:
+    redis_client = RedisClient()
+
     def process_item(self, item, spider):
+        self.redis_client.send_data_to_pipeline(item)
         return item
